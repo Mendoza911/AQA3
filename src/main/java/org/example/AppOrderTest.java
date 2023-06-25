b2b50161b73719199ffc33301fd22e758d0f7cb8
Kirill Markov <isikir668@gmail.com>
2023-06-26 00:21:10 +0300

    Second commit.

--- src/main/java/org/example/AppOrderTest.java
+++ src/main/java/org/example/AppOrderTest.java
@@ -3,39 +3,16 @@ package org.example;
 import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class AppOrderTest {
-    private WebDriver driver = new ChromeDriver();
 
     @BeforeAll
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-    }
-
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--no-sandbox");
-        driver = new ChromeDriver(options);
-    }
-
-    @BeforeEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
     }
 
     @Test
