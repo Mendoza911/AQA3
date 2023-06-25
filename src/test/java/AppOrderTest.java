70721dde6c91fb612df0cc6163cc0d4e52846c2c
Kirill Markov <isikir668@gmail.com>
2023-06-26 00:34:27 +0300

    Fourth commit.

--- src/test/java/AppOrderTest.java
+++ src/test/java/AppOrderTest.java
@@ -1,26 +1,20 @@
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppOrderTest {
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @Test
     void ShouldBeSuccess() {
         open("http://0.0.0.0:9999");
         SelenideElement form = $(".form");
-        form.$("[data-test-id=name] input").setValue("Kirill");
+        form.$("[data-test-id=name] input").setValue("Кирилл");
         form.$("[data-test-id=phone] input").setValue("+79581732626");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=order-success]");
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", $("[data-test-id=order-success]").getText().trim());
     }
 
 }
